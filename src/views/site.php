13629a7f9fa86be3c72c356a54bedb432fab4806
gretanorv <gretanorv@gmail.com>
2021-01-23 15:17:25 +0200

    print pages

--- src/views/site.php
+++ src/views/site.php
@@ -1,3 +1,8 @@
 <?php
 
 print("New page");
+$arr = $entityManager->getRepository('Models\Page')->findBy(array('name' => str_replace("/", "", str_replace("/", "", $request))));
+
+dump($arr);
+
+print($arr[0]->getContent());
